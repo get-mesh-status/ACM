f74785efc7a117ae5d300d2a6c1a08d968dd4db6
Umair Ahmed Shah <umairahmedshah@hotmail.com>
2017-09-24 14:59:40 +0200

    Added the project with C# fundamental course

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    // callable from this solution and other outside, classes are also typed
+    
+    public class Customer
+    {
+
+        public Customer()
+            :this(0)
+        {
+                
+        }
+        public Customer(int customerId )
+        {
+           // this points to current instance of the class
+            this.CustomerId = customerId;
+            AddressList = new List<Address>();
+        }
+
+        public List<Address> AddressList { get; set; }
+        //static modifier ,ensuring that InstanceCount
+        // belong to the Type Customer itself
+        public static int InstanceCount { get;  set; }
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+
+        }
+
+        public string FirstName { get; set; }
+       
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if(!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            } 
+        }
+
+       
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+           
+            Object myobjk = new Object();
+            Console.WriteLine("Object created:" + myobjk.ToString());
+            Console.WriteLine("Customer Object is :" + this.ToString());
+
+            
+            return isValid;
+        }
+        
+    }
+}
---
+++ ACM.BL/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+   public  class Order
+    {
+       public Order()
+       {
+
+       }
+       public Order(int orderId)
+       {
+           this.OrderId = orderId;
+       }
+
+       // a nullable value type, tracks the date,time and timezone offset
+       //e.g. detroit or paris order 10am time will not be placed at the same time
+
+       public DateTimeOffset? OrderDate { get; set; }
+       public int OrderId { get; private set; }
+
+      
+     
+       
+
+       public bool Validate()
+       {
+           var isValid = true;
+           if (OrderDate == null) isValid = false;
+
+           return isValid;
+       }
+
+    }
+}
